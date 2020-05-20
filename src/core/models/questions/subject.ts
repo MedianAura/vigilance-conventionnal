435e2980d6f173a75e3cb9100e3e0b057c3371f8
Sébastien Lafleur <slafleur@vigilance.ca>
2020-05-20 13:51:53 -0400

    change: afficher le sujet dans tous les cas
    toujours afficher le sujet et la description dans un commit qui sera envoyé du commit
    
    [log]

--- src/core/models/questions/subject.ts
+++ src/core/models/questions/subject.ts
@@ -21,9 +21,6 @@ const SubjectQuestion: InputQuestion = {
   transformer(subject) {
     const color = subject.length <= 100 ? chalk.green : chalk.red;
     return color(`(${subject.length}) ${subject}`);
-  },
-  when(answers) {
-    return !answers.log;
   }
 };
 
